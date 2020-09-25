bee33a93fc67d374b98df4277c77d0765be5a274
jonathan-scholbach <j.scholbach@posteo.de>
2020-09-26 01:18:38 +0200

    add typos functionality

--- README.md
+++ README.md
@@ -1,31 +1,62 @@
-__jargon__ is a *minimalistic* vocabulary trainer which lets (and makes) you define your vocabulary on your own. When you train your vocabulary, __jargon__ keeps track of your success on every word, and trains your weakest words most.
+__jargon__ is a minimalistic vocabulary trainer. The main idea of __jargon__ is to make it easy to create and share your custom vocabulary. When you run exercise on your vocabulary, __jargon__ keeps track of your learning progress on every item, and trains your weakest items most.
 
 # Install
 
-In order to install __jargon__, the following prerequisites are required:
+In order to install __jargon__, the following prerequisites have to be met:
 
 + [`git`](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git) (optional, pre-installed on Linux and Mac)
 + [`Python 3.8`](https://www.python.org/downloads/)
 + [`virtualenv`](https://pypi.org/project/virtualenv/) (optional)
 + `pip` (usually installed with Python)
 
-With these prerequisites installed, open a terminal, move to the location you want to install __jargon__ to and type:
+With these prerequisites installed, open a terminal, move to the location you want to install __jargon__ to and perform the following steps:
 
+1. Clone the repository from GitHub (If you do not have `git` installed and do not want to install it, just download the directory from https://github.com/jonathan-scolbach/jargon and skip this step.)
 ```
 git clone git@github.com:jonathan-scholbach/jargon.git
+```
+
+2. Move into the directory:
+```
 cd jargon
+```
+
+3. Create a virtual environment, so you don't need to install the python dependencies globally ...
+```
 virtualenv env --python=python3.8
+```
+
+4. ... and activate it:
+
++ On Linux / macOS:
+
+```
+source env/bin/activate
+```
+ 
++ On Windows:
+
+```
+env\Scripts\activate.bat
+```
+
+If you do not have `virtualenv` installed and do not want to install it, you can skip the creation and activation of the virtual environment. This will install the requirements globally on your machine.
+
+5. Install the requirements:
+
+```
 pip install -r requirements.txt
 ```
 
-If you do not have `git` installed and do not want to install it, just download the directory from https://github.com/jonathan-scolbach/jargon and proceed from the second line. If you do not have `virtualenv` installed and do not want to install it, you can skip the third line. This will install the requirements globally on your machine.
-
 # Create Custom Vocabulary
 
-## Format
-Your custom vocabulary must be put in a [csv](https://en.wikipedia.org/wiki/Comma-separated_values) file which has three columns, separated by semicolon (`";"`). The first column contains the vocabulary in the target language you want to train, the second column contains the translation of the vocabulary into your native language, the third column must be called "success". Keep it empty, it will be used to track your performance for each vocabulary.
+Your custom vocabulary must be put in a [`csv`](https://en.wikipedia.org/wiki/Comma-separated_values) file which has to have two columns, separated by semicolon (`";"`). You can create this file with a simple text editor, or if this is more convenient for you, with a spreadsheet program such as [OpenOffice](https://www.openoffice.org/product/calc.html) Calc, Microsoft Excel or GoogleSheets
 
-If you want to name multiple synonyms in one of the languages, separate them by comma.
+The first column contains the vocabulary in the target language you want to train. The second column contains the translation of the vocabulary into your native language.
+
+If you want to name multiple synonyms in one of the languages, write them into the same cell, separated by comma.
+
+For an example, have a look at the example csv file `example_vocabulary.csv` in the repository.
 
 # Run
 
@@ -55,22 +86,25 @@ For example:
 python train.py ../lessons/advanced_english_from_german.csv 
 ```
 
-If you do not provide the path to your vocabulary, `vocabulary.csv` will be used as a default.
+
+## Options
+
+When you invoke the script, you can set several flags which change the behavior of the exercise:
+
++ `-a` (or, with the same effect: `--alternatives`): This will make the program consider the synonyms as valid answers. If it is not set, all the synonyms have to be named by the user for the answer to be considered valid.
+
++ `-t` (or, with the same effect: `--typo`): This will make the program consider answers valid if they contain a typo.
 
 To finish your exercise, just type `"x"` when prompted for the vocabulary answer.
 
 # Missing Features
 
-__jargon__ is minimalistic, it is written just to provide an easy way to exercise on custom vocabulary. Hence there are some functionalities you might miss:
+__jargon__ is minimalistic, it is written for users who need an easy and quick way to train their specialised vocabulary. There are some functionalities you might miss:
 
 ## Multiple Users
 
-__jargon__ does not have built-in functionality to keep track of learning progress of different users. However, multiple users can track their performance individually by creating separate vocabulary files or directories per user.
+__jargon__ does not have built-in functionality to keep track of learning progress of different users. However, since the learning progress is stored alongside the vocabulary (in the same file), multiple users can track their progress individually by creating separate vocabulary files per user.
 
 ## Lessons
 
 __jargon__ does not separate vocabulary in different lessons. However, you can organize your different lessons in different vocabulary csv files.
-
-## Typo Correction
-
-__jargon__ does not correct for your typos during exercise. Only the very correct word spelling is accepted, as stated in your vocabulary csv file. You have to type correctly.
--- train.py
+++ train.py
@@ -1,10 +1,12 @@
 import argparse
-import copy
 import datetime as dt
 from os import system, name
+import typing as tp
 
 import pandas as pd
 from termcolor import colored as clr
+
+from utils import levenshtein
 
 
 RELEVANT_SUCCESS_SEQUENCE_LENGTH = 5
@@ -21,20 +23,53 @@ def clear():
         _ = system("clear")
 
 
-def exercise(
-    question: str, 
-    solution: str, 
-    treat_synonyms_as_alternatives: bool,
-) -> bool:
-    
-    clear()
-    solutions = [s.strip()  for s in solution.split(",")]
-    remaining_solutions = copy.deepcopy(solutions)
-    print(clr(question, "yellow") + "\n\n")
-    
-    answer_correct = True
+def evaluate_answer(
+    answer: str,
+    valid_answers: tp.List[str],
+    treat_synonyms_as_alternatives: bool = False,
+    allow_typos: bool = False,
+) -> tp.Tuple[tp.List[str], bool]:
+    """Evaluate answer agains possible valid answers."""
+    if allow_typos:
+        for valid_answer in valid_answers:
+            delta = levenshtein(answer, valid_answer)
+            if answer == valid_answer:
+                break
+            elif delta <= 1 or (
+                len(answer) == len(valid_answer) and delta == 2
+            ):
+                print(
+                    clr(
+                        "Beware of typos! The correct answer is: \n\n\t", "cyan"
+                    )
+                    + clr(valid_answer + "\n", "blue")
+                )
+                answer = valid_answer
+                break
 
-    while remaining_solutions:
+    if answer in valid_answers:
+        if treat_synonyms_as_alternatives:
+            return [], True
+        else:
+            valid_answers.remove(answer)
+            return valid_answers, True
+    else:
+        return valid_answers, False
+
+
+def exercise(
+    question: str,
+    solution: str,
+    treat_synonyms_as_alternatives: bool = False,
+    allow_typos: bool = False,
+) -> bool:
+
+    clear()
+    valid_answers = [s.strip() for s in solution.split(",")]
+
+    print(clr(question, "yellow") + "\n\n")
+
+    while valid_answers:
         answer = input()
 
         if answer == "x":
@@ -42,22 +77,34 @@ def exercise(
 
         print("\n")
 
-        if answer in solutions:
-            remaining_solutions.remove(answer)
-            print(clr("CORRECT!", "green"))
-            if treat_synonyms_as_alternatives:
-                break
-            else:
-                print(
-                    clr("Keep on naming synonyms!\n", "blue") 
-                    if remaining_solutions 
+        valid_answers, answer_correct = evaluate_answer(
+            answer=answer,
+            valid_answers=valid_answers,
+            treat_synonyms_as_alternatives=treat_synonyms_as_alternatives,
+            allow_typos=allow_typos,
+        )
+
+        if answer_correct:
+            print(
+                clr("WELL DONE!", "green")
+                + (
+                    clr(" Keep on naming synonyms!\n", "cyan")
+                    if valid_answers
                     else ""
                 )
+            )
 
         else:
-            print(clr("WRONG! Correct answer would have been: ", "red"))
-            for s in remaining_solutions:
-                print(clr(s, "blue"))
+            print(
+                clr(
+                    "OH NO! Correct answer"
+                    f"{'s' if len(valid_answers > 1) else ''}"
+                    "would have been:\n",
+                    "red",
+                )
+            )
+            for s in valid_answers:
+                print(clr("\t" + s, "blue"))
             answer_correct = False
             break
 
@@ -73,33 +120,43 @@ def evaluate(seq):
 
 if __name__ == "__main__":
     start = dt.datetime.now()
-    parser =  argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "file_path", 
+        "file_path",
         type=str,
         help=(
             "Location of the vocabulary csv file, as relative file path "
             "(relative to current working directory)"
         ),
-        action="store"
+        action="store",
     )
 
     parser.add_argument(
-        "-a", "--alternatives",
+        "-a",
+        "--alternatives",
         help=(
             "If this flag is set, synonyms are treated as alternatives. That "
             "means, you need to name only one synonym for your answer to be "
             "considered correct."
         ),
         default=False,
-        action="store_true"
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "-t",
+        "--typos",
+        help=("If this flag is set, typos are accepted as correct answers."),
+        default=False,
+        action="store_true",
     )
 
     parsed = parser.parse_args()
 
-    FILE_PATH = parsed.file_path 
+    FILE_PATH = parsed.file_path
     TREAT_SYNONYMS_AS_ALTERNATIVES = parsed.alternatives
+    ALLOW_TYPOS = parsed.typos
 
     df = pd.read_csv(FILE_PATH, sep=";", skipinitialspace=True).fillna("0")
     df["success"] = df["success"].astype(int).astype(str)
@@ -107,16 +164,16 @@ if __name__ == "__main__":
     RUN = True
 
     while RUN:
-        df.sort_values(
-            by=["success"], key=lambda s: s.map(len), inplace=True
-        )
+        df.sort_values(by=["success"], key=lambda s: s.map(len), inplace=True)
         df.sort_values(
             by=["success"], key=lambda s: s.map(evaluate), inplace=True
         )
         row = df.iloc[0]
-               
+
         try:
-            res = exercise(row[1], row[0], TREAT_SYNONYMS_AS_ALTERNATIVES)
+            res = exercise(
+                row[1], row[0], TREAT_SYNONYMS_AS_ALTERNATIVES, ALLOW_TYPOS
+            )
             print(res)
             row["success"] += str(int(res))
         except TerminateError:
@@ -129,6 +186,7 @@ if __name__ == "__main__":
     print(
         clr(
             f"You spent {int((end - start).total_seconds() / 60)} minutes in a "
-            "useful manner. Bye Bye!", 
-            "blue")
+            "useful manner. Bye Bye!",
+            "blue",
+        )
     )
